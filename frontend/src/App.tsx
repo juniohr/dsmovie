84fa8b3b378eac4faed0b9748812f2028f267489
Josias Nicolau <josias.26@hotmail.com>
2022-01-11 19:57:25 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
-
+import Navbar from './Assets/Img/components/navbar';
+import{ReactComponent as GithubIcon} from './Assets/Img/github.svg'
 
 function App() {
   return (
-    <h1>Página Principal</h1>
+    <Navbar/>
   );
 }
 
